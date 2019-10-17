5c177678408d9a26afa04450c9097bf3d1015965
designcrumble <y13uc147@lnmiit.ac.in>
2019-10-17 13:19:49 +0530

    landing page calio

---
+++ assets/js/scripts.js
@@ -0,0 +1,9 @@
+$(function () {
+    $('.marquee').marquee({
+        duration: 5000,
+         duplicated: true,
+         gap: 00, 
+         direction: 'right',
+         pauseOnHover: true
+    });
+});
